d43ae1eae3f996949586d147d18854b1212f56c7
Xaryki <109338988+Xaryki@users.noreply.github.com>
2023-12-18 17:13:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,15 @@ document.getElementById('captureButton').addEventListener('click', () => {
     capturePhoto();
 });
 
-let capturedImageData = null; // Глобальная переменная для хранения данных изображения
 
 document.addEventListener('DOMContentLoaded', function() {
     if (window.Telegram.WebApp) {
+        // Расширение Mini App до полной высоты
+        window.Telegram.WebApp.expand();
         Telegram.WebApp.MainButton.hide();
 
         const captureButton = document.getElementById('captureButton');
         captureButton.addEventListener('click', function() {
-            // Логика захвата фотографии
-            // ...
-
             capturedImageData = /* захват данных фотографии */;
             Telegram.WebApp.MainButton.show();
             Telegram.WebApp.MainButton.setText('Отправить');
@@ -30,31 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
         Telegram.WebApp.MainButton.onClick(() => {
             if (capturedImageData) {
                 console.log('Кнопка отправить была нажата');
-                // Отправка данных боту
                 Telegram.WebApp.sendData(capturedImageData);
             }
         });
-
-        // Дополнительная логика Mini App
-        // ...
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOMContentLoaded event triggered");
-    if (window.Telegram.WebApp) {
-        // Расширение Mini App до полной высоты
-        window.Telegram.WebApp.expand();
-
-        // Доступ к различным данным
-        // const initData = window.Telegram.WebApp.initData; // Сырые данные
-        // const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe; // Ненадежные данные
-        // const version = window.Telegram.WebApp.version; // Версия Bot API
-        // ...и так далее для других полей
-
-        // Используйте эти данные в соответствии с вашими потребностями
-    }
-});
+let capturedImageData = null; // Глобальная переменная для хранения данных изображения
 
 function showScreen(screenId) {
     document.querySelectorAll('.screen').forEach(screen => {
