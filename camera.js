3dc56dce026dbeb3463eaa4d0040afbbabc0ca8b
Xaryki <109338988+Xaryki@users.noreply.github.com>
2023-12-22 23:56:35 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,67 +1,18 @@
-document.getElementById('captureButton').addEventListener('click', () => {
-
-    //let base64Data = capturePhoto();
-    //let base64Data2 = capturePhoto2();
-
-    //takeScreenshotAndSend();
-
-    const apiUrl = 'https://borisenko-ivan.online:443/api/v1/send/photo';
-    const postData = {
-    // uuid: 1277274408,
-    uuid: window.Telegram.WebApp.initDataUnsafe.user.id,
-    image: takeScreenshotAndSend(),
-    };
-
-    // Создаем объект XMLHttpRequest
-    const xhr = new XMLHttpRequest();
-
-    // Устанавливаем метод запроса и URL
-    xhr.open('POST', apiUrl, true);
-
-    // Устанавливаем заголовок Content-Type
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    // Обработчик события готовности запроса
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        // Проверяем статус ответа
-        if (xhr.status === 200) {
-        console.log('Успешный ответ от сервера:', xhr.responseText);
-        } else {
-        console.error('Ошибка при отправке запроса. Статус:', xhr.status);
-        }
-    }
-    };
-
-    // Преобразуем объект postData в JSON и отправляем в теле запроса
-    const postDataJSON = JSON.stringify(postData);
-    xhr.send(postDataJSON);
-});
-
-
-
 let faceapi;
 let detections = [];
 
 let video;
 let canvas;
 let hatImg; // Переменная для изображения шляпы
-let styleNumber ;
 
 function preload() {
-
-  styleNumber = localStorage.getItem('selectedStyle');
-
-  hatImg = loadImage('hat2.png'); // Путь к изображению для стиля 2
-
-  Telegram.WebApp.expand();
-
+  // Загружаем изображение шляпы
+  hatImg = loadImage('hat.png'); // Убедитесь, что указан правильный путь к изображению
 }
 
 
 function setup() {
-
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(480, 360);
   canvas.id("canvas");
 
   video = createCapture(VIDEO);
@@ -92,15 +43,30 @@ function gotFaces(error, result) {
   }
 
   detections = result;　//Now all the data in this detections
+  // console.log(detections);
 
   clear();//Draw transparent background;
+  drawBoxs(detections);//Draw detection box:
   drawLandmarks(detections);//// Draw all the face points:
+  // drawExpressions(detections, 20, 250, 14);//Draw face expression:
 
+  // Вставьте вызов функции здесь
   drawHat(detections);
 
   faceapi.detect(gotFaces);// Call the function again at here:
 }
 
+function drawBoxs(detections){
+  if (detections.length > 0) {//If at least 1 face is detected:
+    for (f=0; f < detections.length; f++){
+      let {_x, _y, _width, _height} = detections[f].alignedRect._box;
+      stroke(44, 169, 225);
+      strokeWeight(1);
+      noFill();
+      rect(_x, _y, _width, _height);
+    }
+  }
+}
 
 function drawLandmarks(detections){
   if (detections.length > 0) {//If at least 1 face is detected:
@@ -115,24 +81,33 @@ function drawLandmarks(detections){
   }
 }
 
+function drawExpressions(detections, x, y, textYSpace){
+  if(detections.length > 0){//If at least 1 face is detected:
+    let {neutral, happy, angry, sad, disgusted, surprised, fearful} = detections[0].expressions;
+    textFont('Helvetica Neue');
+    textSize(14);
+    noStroke();
+    fill(44, 169, 225);
 
-
-function drawHat(detections){
-  for (let i = 0; i < detections.length; i++) {
-    let {_x, _y, _width, _height} = detections[i].alignedRect._box;
-
-    // Расчет размера и позиции шляпы
-    let hatWidth = _width * 3;
-    let hatHeight = hatWidth * 0.6; // Примерное соотношение ширины к высоте
-    let hatX = _x - hatWidth * 0.35;
-    let hatY = _y - hatHeight * 0.5; // Смещение шляпы вверх от головы
-
-    // Отрисовка шляпы
-    image(hatImg, hatX, hatY, hatWidth, hatHeight);
+    text("neutral:       " + nf(neutral*100, 2, 2)+"%", x, y);
+    text("happiness: " + nf(happy*100, 2, 2)+"%", x, y+textYSpace);
+    text("anger:        " + nf(angry*100, 2, 2)+"%", x, y+textYSpace*2);
+    text("sad:            "+ nf(sad*100, 2, 2)+"%", x, y+textYSpace*3);
+    text("disgusted: " + nf(disgusted*100, 2, 2)+"%", x, y+textYSpace*4);
+    text("surprised:  " + nf(surprised*100, 2, 2)+"%", x, y+textYSpace*5);
+    text("fear:           " + nf(fearful*100, 2, 2)+"%", x, y+textYSpace*6);
+  }else{//If no faces is detected:
+    text("neutral: ", x, y);
+    text("happiness: ", x, y + textYSpace);
+    text("anger: ", x, y + textYSpace*2);
+    text("sad: ", x, y + textYSpace*3);
+    text("disgusted: ", x, y + textYSpace*4);
+    text("surprised: ", x, y + textYSpace*5);
+    text("fear: ", x, y + textYSpace*6);
   }
 }
 
-function drawHat2(detections){
+function drawHat(detections){
   for (let i = 0; i < detections.length; i++) {
     let {_x, _y, _width, _height} = detections[i].alignedRect._box;
 
@@ -147,62 +122,22 @@ function drawHat2(detections){
   }
 }
 
-function capturePhoto() {
-    const videoElement = document.getElementById('video');
-    const canvas = document.createElement('canvas');
-    canvas.width = videoElement.videoWidth;
-    canvas.height = videoElement.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth , videoElement.videoHeight);
-    //ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
-
-    // Конвертация в Base64
-    const base64Image = canvas.toDataURL('image/png', 1);
-
-    // Удаление префикса Base64
-    const base64Data = base64Image.split(',')[1];
-    //console.log(base64Image);
-    return base64Data;
-}
-
-
-function capturePhoto2() {
-  // Предполагаем, что у вашего элемента canvas есть id "canvas"
-  let canvasElement = document.getElementById('canvas');
-
-  if (canvasElement) {
-    // Преобразуем содержимое канваса в строку Base64
-    let base64Image = canvasElement.toDataURL('image/png', 1);
-    // Удаление префикса Base64
-    const base64Data = base64Image.split(',')[1];
-    // Выводим строку Base64 в консоль
-    //console.log(base64Data);
-    return base64Data;
-  } else {
-    console.log('Канвас не найден');
+function keyTyped(){
+  if (key === 's') {
+    saveFrames('detection', 'png', 1, 25);
   }
-
 }
 
-
-function takeScreenshotAndSend() {
-    let videoElement = document.getElementById('video');
-    let canvasElement = document.getElementById('canvas');
-
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.id("final");
-
-    const ctx = canvasElement.getContext('2d');
-    ctx.clearRect(0,0, videoElement.videoWidth , videoElement.videoHeight)
-
-
-    ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth , videoElement.videoHeight);
-    drawHat2(detections);
-
-    console.log(canvasElement.toDataURL('image/png'));
-    return canvasElement.toDataURL('image/png').split(',')[1];
+function shadow1(){
+  drawingContext.shadowOffsetX = 1;
+  drawingContext.shadowOffsetY = -1;
+  drawingContext.shadowBlur = 6;
+  drawingContext.shadowColor = 'rgba(255, 255, 255, 100)';
 }
 
-
-
-
+function shadow2(){
+  drawingContext.shadowOffsetX = 1;
+  drawingContext.shadowOffsetY = -1;
+  drawingContext.shadowBlur = 18;
+  drawingContext.shadowColor = 'rgba(255, 255, 255, 100)';
+}
